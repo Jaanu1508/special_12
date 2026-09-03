c5283cdd4b132eac99c42bc981a44ecb56e5064c
Jahnavi <jahnavichowdary15082008@gmail.com>
2026-09-03 23:49:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const heartsContainer = document.querySelector(".hearts");
 
-const hearts = ["❤️", "🤍"];
+const heartSymbols = ["❤️", "🤍"];
 
 for (let i = 0; i < 100; i++) {
-    const heart = document.createElement("span");
+    const heart = document.createElement("div");
 
     heart.className = "heart";
 
     heart.textContent =
-        hearts[Math.floor(Math.random() * hearts.length)];
+        heartSymbols[Math.floor(Math.random() * heartSymbols.length)];
 
-    heart.style.left = Math.random() * 100 + "%";
-    heart.style.fontSize = (12 + Math.random() * 25) + "px";
-    heart.style.animationDuration = (6 + Math.random() * 8) + "s";
-    heart.style.animationDelay = -(Math.random() * 10) + "s";
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.fontSize = (12 + Math.random() * 28) + "px";
+    heart.style.animationDuration = (5 + Math.random() * 10) + "s";
+    heart.style.animationDelay = -(Math.random() * 15) + "s";
 
     heartsContainer.appendChild(heart);
 }
