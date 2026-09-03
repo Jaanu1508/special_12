a0a2b7c0b1d39f0c06e2702357053a362f581fd9
Jahnavi <jahnavichowdary15082008@gmail.com>
2026-09-03 23:45:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const heartsContainer = document.querySelector(".hearts");
 
+const hearts = ["❤️", "🤍"];
+
+for (let i = 0; i < 100; i++) {
+    const heart = document.createElement("span");
+
+    heart.className = "heart";
+
+    heart.textContent =
+        hearts[Math.floor(Math.random() * hearts.length)];
+
+    heart.style.left = Math.random() * 100 + "%";
+    heart.style.fontSize = (12 + Math.random() * 25) + "px";
+    heart.style.animationDuration = (6 + Math.random() * 8) + "s";
+    heart.style.animationDelay = -(Math.random() * 10) + "s";
+
+    heartsContainer.appendChild(heart);
+}
